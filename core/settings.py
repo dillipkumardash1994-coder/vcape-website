0953af6a0ef1396f5505c8f18715e6c01230f0f5
Dillip <dillipkumardash63@gmail.com>
2026-02-16 15:35:55 +0530

    the latest change

--- core/settings.py
+++ core/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-d7nz8qn0abmiux6o=cur5ec5&=wj(!9lu_k#q-m3se61ng&nyj'
-SECRET_KEY = os.environ.get('SECRET_KEY') #Added before deployment in internet
+SECRET_KEY = 'django-insecure-d7nz8qn0abmiux6o=cur5ec5&=wj(!9lu_k#q-m3se61ng&nyj'
+#SECRET_KEY = os.environ.get('SECRET_KEY') #Added before deployment in internet
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",  # ✅ MOVE HERE
-
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -83,21 +82,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'vcape_db',
-#         'USER': 'postgres',
-#         'PASSWORD': '12345',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vcape_db',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL')
+#     )
+# }
 
 
 # Password validation
